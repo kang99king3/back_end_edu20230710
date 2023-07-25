441168992340e269a04cd118da5b2b67d7d01a9a
hankyung <hankyung@bjkang>
2023-07-25 11:31:53 +0900

    변수의 형변환코드 추가

--- backend_javaproject_2023/src/hk/edu20230725/day02/D1_VariableTest.java
+++ backend_javaproject_2023/src/hk/edu20230725/day02/D1_VariableTest.java
@@ -20,7 +20,27 @@ public class D1_VariableTest {
 		int i=5000000;//4byte크기 <---java에서 정수 기본타입
 		long l=500000000000L;//int크기를 벗어나면 오류--> L을 붙여줌
 		
+		//2.실수형
+		double d=15.8;
+//		float f=30.15F;
+		float ff=(float)40.2;// <---실수의 기본형은 double타입이다.
 		
+		//3.다른 타입끼리 연산
+		int ii=(int)(i+d); //int + double
+		int iii=i+(int)d;
+		double dd=i+d;
+		System.out.println(ii+":"+dd);
 		
+		//4.정수형끼리 연산
+		byte bb=10;
+		byte bb2=20;
+		byte resultB =(byte)(bb+bb2); // <--b+b 기본 int형으로 변환하여 연산
+		
+		int i2=40;
+		int i3=i2+bb;
 	}
 }
+
+
+
+
