3a1dbbbd04bde9c4a171a189a5a10abd506d72ac
hankyung <hankyung@bjkang>
2023-08-01 11:30:47 +0900

    singleton 객체 비교 추가

--- backend_javaproject_2023/src/hk/edu20230801/day07/D2_SingletonMain.java
+++ backend_javaproject_2023/src/hk/edu20230801/day07/D2_SingletonMain.java
@@ -8,10 +8,30 @@ public class D2_SingletonMain {
 		//              생성자에 접근한다.--> 접근 못하게 하려면?
 //		SingletonTest single=new SingletonTest();//접근못해서 실행할 수 없다(강제)
 //		SingletonTest single=getInstance();//non-static 메서드: 객체명.메서드() 호출
-		D2_SingletonTest single=D2_SingletonTest.getInstance();
+		D2_SingletonTest single1=D2_SingletonTest.getInstance();
+		
+		//정말 한번만 생성되는지 확인해보자
+		D2_SingletonTest single2=D2_SingletonTest.getInstance();
+		D2_SingletonTest single3=D2_SingletonTest.getInstance();
+		D2_SingletonTest single4=D2_SingletonTest.getInstance();
+		
+		System.out.println(single1==single2);//주소로 비교해보기
+		System.out.println(single1.equals(single2));//hashcode로 비교해보기
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
