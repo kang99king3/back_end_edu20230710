eb14ac053b6ce218c2b5c3b6c0e289260dfd5a18
hankyung <hankyung@bjkang>
2023-08-02 14:09:52 +0900

    calculator 클래스 완성하기

---
+++ backend_javaproject_2023/src/hk/edu20230802/day08/CalculatorMain.java
@@ -0,0 +1,24 @@
+package hk.edu20230802.day08;
+
+public class CalculatorMain {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("계산 값을 입력하세요(+,-*,/ 연산만 가능)입력은 \"5+10\"");
+//		Calculator 객체생성
+//		Scanner 객체생성
+
+		while(true) {
+			System.out.println("계산 값을 입력하세요(+,-*,/ 연산만 가능)입력은 \"5+10\"");
+			//예: "5+10"입력값 받는 코드 작성
+//			String s=scan.next();
+			//"9"를 입력하면 "계산 프로그램을 종료해요~~" 출력하고 끝내기 코드 작성
+//			if(s.equals("9")) {
+//				System.out.println("계산 프로그램을 종료해요~~~");
+//				break;
+//			}
+			//계산실행하기 메서드 호출: 객체명.메서드명	
+			//cal.메서드명(s);
+		}
+	}
+}
