32b29c7a31c7c64faee9b7eefa16489a6c7f10a6
hankyung <hankyung@bjkang.mshome.net>
2023-11-08 09:55:33 +0900

    일정추가 일부분 작성

--- 16_schedule_springbootMVC/src/main/java/com/hk/calboard/controller/CalController.java
+++ 16_schedule_springbootMVC/src/main/java/com/hk/calboard/controller/CalController.java
@@ -2,12 +2,15 @@ package com.hk.calboard.controller;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hk.calboard.command.InsertCalCommand;
 import com.hk.calboard.service.ICalService;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,19 +18,36 @@ import jakarta.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping(value="/schedule")
 public class CalController {
- 
+	//log를 원하는 위치에 설정하여 디버깅 하기
+	private static final Logger logger=LoggerFactory.getLogger(CalController.class);
+	
 	@Autowired
 	private ICalService calService;
 	
 	@GetMapping(value="/calendar")
 	public String calendar(Model model, HttpServletRequest request) {
-		 
+		logger.info("달력보기"); 
 		Map<String, Integer>map=calService.makeCalendar(request);
 		model.addAttribute("calMap", map);
 		return "thymeleaf/calboard/calendar";
 	}
 	
+	@GetMapping(value = "/addCalBoardForm")
+	public String addCalBoardForm(Model model, InsertCalCommand insertCalCommand) {
+		logger.info("일정추가폼이동");
+		System.out.println(insertCalCommand);
+		//addCalBoardfForm 페이지에서 유효값 처리를 위해 insertCalCommand 받고 있기때문에
+		model.addAttribute("insertCalCommand", insertCalCommand);
+		return "thymeleaf/calboard/addCalBoardForm";
+	}
+	
 }
+
+
+
+
+
+
 
 
 
