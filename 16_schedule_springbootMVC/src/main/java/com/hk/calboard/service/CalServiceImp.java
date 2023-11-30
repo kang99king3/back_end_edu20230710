dd5ecafb7954b1670e417fd9ddfb2a7d03d4f766
hankyung <hankyung@bjkang>
2023-11-30 16:48:16 +0900

    CalController에 clist코드를 CalService에 makeCalendar()로 옮겨서 2024-1에서 2023-12로 이동시 일정 못가져오는 부분 수정

--- 16_schedule_springbootMVC/src/main/java/com/hk/calboard/service/CalServiceImp.java
+++ 16_schedule_springbootMVC/src/main/java/com/hk/calboard/service/CalServiceImp.java
@@ -61,6 +61,12 @@ public class CalServiceImp implements ICalService{
 		//2.월의 마지막 날 구하기
 		int lastDay=cal.getActualMaximum(Calendar.DAY_OF_MONTH);
 		
+		String id="kbj";
+		//makeCalendar()에서 실행해야 12->1   1->12 처리시 정상실행할 수 있음
+				String yyyyMM=year+Util.isTwo(month+"");//202311 6자리변환
+				List<CalDto>clist=calViewList(id, yyyyMM);
+		request.setAttribute("clist", clist);
+				
 		map.put("year", year);
 		map.put("month", month);
 		map.put("dayOfWeek", dayOfWeek);
