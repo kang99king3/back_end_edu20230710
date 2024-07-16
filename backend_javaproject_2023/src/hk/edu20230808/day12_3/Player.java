f107ae92bf8f1ba7bc8d98c9d2601123c5790d24
hankyung <hankyung@bjkang>
2024-07-16 09:11:23 +0900

    카드게임 소스 추가

---
+++ backend_javaproject_2023/src/hk/edu20230808/day12_3/Player.java
@@ -0,0 +1,16 @@
+package hk.edu20230808.day12_3;
+
+import java.util.ArrayList;
+
+public class Player {
+
+	private ArrayList<Card> player;
+	
+	public Player() {
+		player=new ArrayList<Card>();
+	}
+	
+	public ArrayList<Card> getPlayer() {
+		return player;
+	}
+}
