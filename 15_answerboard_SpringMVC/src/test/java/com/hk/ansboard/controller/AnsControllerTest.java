693cc3f52df3e158fe35377f6b16c1e9e0310802
bjkang <kang99king3@gmail.com>
2024-10-08 16:28:48 +0900

    추가

--- 15_answerboard_SpringMVC/src/test/java/com/hk/ansboard/controller/AnsControllerTest.java
+++ 15_answerboard_SpringMVC/src/test/java/com/hk/ansboard/controller/AnsControllerTest.java
@@ -84,6 +84,26 @@ public class AnsControllerTest {
 		//200코드와 같은지를 비교하기---> 200은 성공, 404는 경로에러, 500은 코드 오류
 		assertEquals(HttpStatus.OK.value(), statusCode);
 	}
+	
+	@Test
+	public void testInsertBoard() throws Exception {
+		// mock 객체를 활용해서 가상의 클라이언트 요청
+		this.mock=MockMvcBuilders.webAppContextSetup(wac).build();
+		
+		//boardList.do를 요청해보자~~
+		MvcResult result=mock.perform(MockMvcRequestBuilders.post("/insertBoard.do")
+									  .param("id", "hp").param("title", "제목")
+									  .param("content", "내용")
+				                      )
+				             .andExpect(MockMvcResultMatchers.status().isOk())
+						     .andReturn();
+		
+		int statusCode=result.getResponse().getStatus();//결과내용을 받아옴
+		System.out.println("status 코드:"+statusCode);
+		System.out.println("HttpStatus:"+HttpStatus.OK.value());
+		//200코드와 같은지를 비교하기---> 200은 성공, 404는 경로에러, 500은 코드 오류
+		assertEquals(HttpStatus.OK.value(), statusCode);
+	}
 }
 
 
